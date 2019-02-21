1c44122e7c1de1c72364362fc4f344457fe0b6b9
msurminen <surminen@gmail.com>
2019-02-21 11:00:39 +1100

    Added capability to add and answer questions, with auth0 authentication.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import NavBar from './NavBar/NavBar';
 import {Route} from 'react-router-dom';
 import Questions from './Questions/Questions';
 import Question from './Question/Question';
+import Callback from './Callback/Callback';
+import NewQuestion from './NewQuestion/NewQuestion';
+import SecuredRoute from './SecuredRoute/SecuredRoute';
 
 class App extends Component {
   render() {
@@ -13,6 +16,10 @@ class App extends Component {
 
         <Route exact path='/' component={Questions}/>
         <Route exact path='/question/:questionId' component={Question}/>
+
+        <Route exact path='/callback' component={Callback}/>
+
+        <SecuredRoute path='/new-question' component={NewQuestion} />
 
       </div>
     );
