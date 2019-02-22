e6549caaeb09c56023406d41799e9826518b893b
msurminen <surminen@gmail.com>
2019-02-22 19:10:49 +1100

    Now works with neo4j. Only one answer per question though.

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,15 +26,18 @@ instance.model('Question', {
     },
     answers: {
         type: 'list',
+    },
+    author: {
+        type: 'string',
     }
 });
 
-instance.create('Question', {
-    title: 'Test title', answers: ['ans1', 'ans2']
-})
-.then(question => {
-    console.log(question.get('answers')[0]); // 'Adam'
-});
+//instance.create('Question', {
+//    title: 'Test title', answers: ['ans1', 'ans2']
+//})
+//.then(question => {
+//    console.log(question.get('answers')[0]); // 'Adam'
+//});
 
 // define the Express app
 const app = express();
@@ -62,15 +65,52 @@ app.get('/', (req, res) => {
     description: q.description,
     answers: q.answers.length,
   }));
-  res.send(qs);
+
+  instance.all('Question')
+    .then(collection => {
+        const foo = collection.map(q => ({id: q.get('question_id'), 
+                title: q.get('title'), description: q.get('description'),
+                answers: q.get('answers').length, author: q.get('author')}));
+        console.log(collection.length);
+        res.send(foo);
+
+    })
+
+
+    console.log('xx');
+    console.log(questions);
+
+//res.send(qs);
+//  res.send(foo);
 });
 
 // get a specific question
 app.get('/:id', (req, res) => {
-  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-  if (question.length > 1) return res.status(500).send();
-  if (question.length === 0) return res.status(404).send();
-  res.send(question[0]);
+//  const question = questions.filter(q => (q.id === req.params.id));
+//   if (question.length > 1) return res.status(500).send();
+//   if (question.length === 0) return res.status(404).send();
+
+  console.log('yy')
+  instance.find('Question', req.params.id)
+    .then(q => {
+        console.log(q.get('title')); // 1
+
+        try {
+            r = JSON.parse(q.get('answers'));
+        } catch ({error}) {
+            r = "";
+        }
+
+        const foo = {id: q.get('question_id'), 
+        title: q.get('title'), description: q.get('description'),
+        answers: [r], author: q.get('author')};
+
+        console.log(foo);
+
+        res.send(foo);
+    });
+
+//   res.send(question[0]);
 });
 
 const checkJwt = jwt({
@@ -90,19 +130,25 @@ const checkJwt = jwt({
 // insert a new question
 app.post('/', checkJwt, (req, res) => {
     const {title, description} = req.body;
-    const newQuestion = {
-      id: questions.length + 1,
-      title,
-      description,
-      answers: [],
-      author: req.user.name,
-    };
-    questions.push(newQuestion);
 
-    instance.create('Question', {
-        title: title, description: description
-    })
-    
+    const x = instance.create('Question', {
+        title: title, description: description, answers: [], author: req.user.name
+        // title: title, description: description, answers: [JSON.stringify({answer: 'Nothing yet', author: ''})], author: req.user.name
+    }).then(q => {
+        const newQuestion = {
+            //        id: questions.length + 1,
+                    id: q.get('question_id'),
+                    title,
+                    description,
+                    answers: [],
+                    // answers: [JSON.stringify({answer: 'Nothing yet', author: ''})],
+                    author: req.user.name,
+                };
+        questions.push(newQuestion);
+            
+    });
+
+  
     res.status(200).send();
   });
   
@@ -110,15 +156,41 @@ app.post('/', checkJwt, (req, res) => {
   app.post('/answer/:id', checkJwt, (req, res) => {
     const {answer} = req.body;
   
-    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-    if (question.length > 1) return res.status(500).send();
-    if (question.length === 0) return res.status(404).send();
-  
-    question[0].answers.push({
-      answer,
-      author: req.user.name,
+    console.log(answer);
+    console.log(req.params.id);
+
+    instance.find('Question', req.params.id)
+    .then(q => {
+        console.log('zz');
+        console.log(q);
+        q.update({question_id: req.params.id, answers: [JSON.stringify({answer: answer, author: req.user.name})], author: req.user.name});
     });
+
+    // const qs = questions.map(q => ({
+    //     id: q.id,
+    //     title: q.title,
+    //     description: q.description,
+    //     answers: q.answers.length,
+    //   }));
+
+    // console.log('questions: ' + qs[0].id);
+    // console.log('id: ' + req.params.id);
+
+    
+    // const question = questions.filter(q => (q.id === req.params.id));
+    // console.log('question length: ' + question.length);
+    // if (question.length > 1) return res.status(500).send();
+    // if (question.length === 0) return res.status(404).send();
   
+    // question[0].answers.push({
+    //   answer,
+    //   author: req.user.name,
+    // });
+  
+    // console.log('question: ' + question[0].title);
+    // console.log('question: ' + question[0].answers[0].answer);
+    // console.log('question: ' + question[0]);
+
     res.status(200).send();
   });
 
